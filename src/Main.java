1980eac2710ef2c75f60297bd11b26e95ef35734
taohuayu <2251221623@qq.com>
2023-01-18 09:44:30 +0800

    taohuayu修改

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World! 修改11111");
          System.out.println("Hello World! 修改222222");
-        System.out.println("git-study");
     }
 }
